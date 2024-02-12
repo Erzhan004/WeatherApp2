a4aea8d611a3cfd1566862223a7a5120062b4248
Erzhan004 <124499458+Erzhan004@users.noreply.github.com>
2024-02-12 17:07:42 +0600

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
-
+app.set('views',path.join(process.cwd(), 'views'));
 var indexRouter = require("./routes/index");
 app.use('/',indexRouter);
 
